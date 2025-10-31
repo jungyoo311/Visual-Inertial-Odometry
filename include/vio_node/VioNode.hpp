bbfbf7abe755fc925151a5485c2e7a4861ff42ad
Jeonghyun Jo <jeonghyunjo@berkeley.edu>
2025-10-31 00:17:31 -0700

    [fix]: [Refactored main structure] - Refactored main pkg structure - Addressed sensor callback step - TODO: sensor sync has to be fixed

--- include/vio_node/VioNode.hpp
+++ include/vio_node/VioNode.hpp
@@ -1,22 +1,38 @@
 #ifndef VIO_NODE
 #define VIO_NODE
+
+// ROS headers
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-// #include <tf2_ros/transform_broadcaster.h>
-#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include "std_msgs/msg/int32.hpp"
+
+// STD headers
 #include <queue>
+#include <map>
+#include <thread>
+#include <mutex>
+
+// Library headers
+#include <opencv2/opencv.hpp>
 #include <eigen3/Eigen/Dense>
+
+// Custom headers
 #include "../include/core/VioEstimator.hpp"
 #include "../utils/measure_time.hpp"
-#include "std_msgs/msg/int32.hpp"
-#include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/msg/transform_stamped.h>
+
+#define IMAGE0_TOPIC "/cam0/image_raw"
+#define IMAGE1_TOPIC "/cam1/image_raw"
+#define IMU_TOPIC "/imu0"
+
 using namespace std;
 
 class VioNode : public rclcpp::Node
@@ -25,30 +41,26 @@ class VioNode : public rclcpp::Node
 
         VioNode();
         ~VioNode();
-        /**
-         * @brief Process a synchronized set of IMU and image measurements
-        */
-        // extract two images with the same t from two different topics
-        void img_cb(const sensor_msgs::msg::Image::ConstSharedPtr& cam0_msg, const sensor_msgs::msg::Image::ConstSharedPtr& cam1_msg);
+
     private:
         //ROS subscribers, publishers
-        rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr sub_imu0;
-        message_filters::Subscriber<sensor_msgs::msg::Image> sub_cam0;
-        message_filters::Subscriber<sensor_msgs::msg::Image> sub_cam1;
+        std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> sub_left_;
+        std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> sub_right_;
+        std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Imu>> sub_imu_;
 
         // vio
         // rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub;
-        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub;
-        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr blue_dots_pub;
-        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr green_dots_pub;
-        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr red_dots_pub;
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub_;
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr blue_dots_pub_;
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr green_dots_pub_;
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr red_dots_pub_;
 
-        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odometry;
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odometry_;
 
         rclcpp::TimerBase::SharedPtr pub_timer;
-        std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
+        std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
         // stereo img synchronizer
-        std::shared_ptr<message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image>>> sync;
+        std::shared_ptr<message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Imu>>> sync;
 
         VioEstimator estimator;
         cv::Mat disparityMap;
@@ -64,7 +76,17 @@ class VioNode : public rclcpp::Node
         std::mutex m_buf;
         queue<pair<double, Eigen::Vector3d>> accBuf;
         queue<pair<double, Eigen::Vector3d>> gyrBuf;
-        // rclcpp::TimerBase::SharedPtr timer_;
+
+        void setPubSub();
+
+        void setParams();
+
+        /**
+         * @brief Process a synchronized set of IMU and image measurements
+        */
+        // extract two images with the same t from two different topics
+        void sensor_cb(const sensor_msgs::msg::Image::ConstSharedPtr& cam0_msg, const sensor_msgs::msg::Image::ConstSharedPtr& cam1_msg, const sensor_msgs::msg::Imu::ConstSharedPtr& imu_msg);
+        
         void publish_odometry();
         /**
          * @brief Put IMU info and push them into a queue(buffer)
--- src/VioNode.cpp
+++ src/VioNode.cpp
@@ -1,49 +1,92 @@
-#include <map>
-#include <thread>
-#include <mutex>
-
-#include <rclcpp/rclcpp.hpp>
-#include <opencv2/opencv.hpp>
-#include <cv_bridge/cv_bridge.h>
-#define IMAGE0_TOPIC "/cam0/image_raw"
-#define IMAGE1_TOPIC "/cam1/image_raw"
-#define IMU_TOPIC "/imu0"
 #include "../include/vio_node/VioNode.hpp"
+#include <rclcpp/rclcpp.hpp>
 
 VioNode::VioNode() : Node("VIO_estimator")
 {
-    // imu queue depth 50 seems enough?
-    rclcpp::QoS imu_qos(rclcpp::KeepLast(50));
-    imu_qos.best_effort();
-    sub_imu0 = this -> create_subscription<sensor_msgs::msg::Imu>(IMU_TOPIC, imu_qos, 
-                                                                    std::bind(&VioNode::imu0_callback, this, std::placeholders::_1));
-    rclcpp::QoS qos = rclcpp::QoS(rclcpp::KeepLast(100));
-    sub_cam0.subscribe(this, IMAGE0_TOPIC, qos.get_rmw_qos_profile());
-    sub_cam1.subscribe(this, IMAGE1_TOPIC, qos.get_rmw_qos_profile());
-    uint32_t q_size = 10;
-    //initialize synchronizer - Policy: Approximate time 3ms
-    sync = std::make_shared<message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image>>>(
-        message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image>(q_size), sub_cam0, sub_cam1);
-    sync->setAgePenalty(0.003);
-    sync->registerCallback(std::bind(&VioNode::img_cb, this, std::placeholders::_1, std::placeholders::_2));
+    // Image Synchronizer
+    sub_left_ = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, IMAGE0_TOPIC);
+    sub_right_ = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, IMAGE1_TOPIC);
+    sub_imu_ = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Imu>>(this, IMU_TOPIC);
+    using syncpolicy = message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Imu>;
+    sync = std::make_shared<message_filters::Synchronizer<syncpolicy>>(
+        syncpolicy(10), *sub_left_, *sub_right_, *sub_imu_);
+    sync->setMaxIntervalDuration(rclcpp::Duration::from_seconds(0.00001));
+    sync->registerCallback(std::bind(&VioNode::sensor_cb, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
     
-    
-    img_pub = this->create_publisher<sensor_msgs::msg::Image>("vio/pose", 10);
-
-    blue_dots_pub = this->create_publisher<std_msgs::msg::Int32>("vio/stats/blue_dots", 10);
-    green_dots_pub = this->create_publisher<std_msgs::msg::Int32>("vio/stats/green_dots", 10);
-    red_dots_pub = this->create_publisher<std_msgs::msg::Int32>("vio/stats/red_dots", 10);
-
-    pub_odometry = this->create_publisher<nav_msgs::msg::Odometry>("odometry", 50);
-    tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-
-    // Create a timer to publish odometry at 30Hz (approx 33ms)
-    pub_timer = this->create_wall_timer(std::chrono::milliseconds(33), 
-                                         std::bind(&VioNode::publish_odometry, this));
+    setPubSub();
+    setParams();
 }
-VioNode::~VioNode(){};
+VioNode::~VioNode(){}
 
-VioEstimator estimator;
+void VioNode::setPubSub() {
+    img_pub_ = this->create_publisher<sensor_msgs::msg::Image>("vio/pose", 10);
+
+    blue_dots_pub_ = this->create_publisher<std_msgs::msg::Int32>("vio/stats/blue_dots", 10);
+    green_dots_pub_ = this->create_publisher<std_msgs::msg::Int32>("vio/stats/green_dots", 10);
+    red_dots_pub_ = this->create_publisher<std_msgs::msg::Int32>("vio/stats/red_dots", 10);
+
+    pub_odometry_ = this->create_publisher<nav_msgs::msg::Odometry>("odometry", 50);
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+    return;
+}
+
+void VioNode::setParams() {
+    return;
+}
+
+void VioNode::sensor_cb(const sensor_msgs::msg::Image::ConstSharedPtr& cam0_msg, 
+    const sensor_msgs::msg::Image::ConstSharedPtr& cam1_msg, 
+    const sensor_msgs::msg::Imu::ConstSharedPtr& imu_msg)
+{      
+    rclcpp::Time left_time = cam0_msg->header.stamp;
+    rclcpp::Time right_time = cam1_msg->header.stamp;
+    rclcpp::Time imu_time = imu_msg->header.stamp;
+    double time_diff = std::abs(left_time.nanoseconds() - imu_time.nanoseconds());
+
+    RCLCPP_INFO(this->get_logger(), 
+      "Received synchronized pair! Time diff: %.10f s, left: %.10f s, right: %.10f s, imu: %.10f s", time_diff / 1e9, 
+      left_time.nanoseconds() / 1e9, right_time.nanoseconds() / 1e9, imu_time.nanoseconds() / 1e9);
+
+    cv_bridge::CvImagePtr cam0_ptr = cv_bridge::toCvCopy(cam0_msg, sensor_msgs::image_encodings::MONO8);
+    cv_bridge::CvImagePtr cam1_ptr = cv_bridge::toCvCopy(cam1_msg, sensor_msgs::image_encodings::MONO8);
+    cv::Mat img0, img1;
+    img0 = cam0_ptr->image;
+    img1 = cam1_ptr->image;
+    time = cam0_msg->header.stamp.sec + cam0_msg->header.stamp.nanosec * (1e-9);
+    cv::Mat visualized_image;
+    stopwatch.start_time();       
+    visualized_image = estimator.inputImage(time, img0, img1);
+    elasped_time = stopwatch.end_time();
+    double processing_time_s = elasped_time / 1000.0;
+    double processing_hz = 1.0 / processing_time_s;
+    RCLCPP_INFO(this->get_logger(), 
+                "VIO Image Processing Time: %.2f ms (%.2f Hz)", 
+                elasped_time,
+                processing_hz);
+    cv_bridge::CvImage out_msg;
+    out_msg.header = cam0_msg->header;
+    out_msg.encoding = sensor_msgs::image_encodings::BGR8; //MONO8
+    out_msg.image = visualized_image;
+    
+    // img_pub_->publish(*cam1_msg);
+    img_pub_->publish(*out_msg.toImageMsg());
+    // publish for RQT PLOT
+    auto stats = estimator.getLastFeatureStats();
+    auto blue_msg = std_msgs::msg::Int32();
+    auto green_msg = std_msgs::msg::Int32();
+    auto red_msg = std_msgs::msg::Int32();
+
+    blue_msg.data = stats.blue_count;
+    green_msg.data = stats.green_count;
+    red_msg.data = stats.red_count;
+
+    blue_dots_pub_->publish(blue_msg);
+    green_dots_pub_->publish(green_msg);
+    red_dots_pub_->publish(red_msg);
+}
+
+VioEstimator estimator; //TODO: move this to class member
 void VioNode::imu0_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
     double t = msg->header.stamp.sec + msg->header.stamp.nanosec * (1e-9);
@@ -86,7 +129,7 @@ void VioNode::publish_odometry()
     odometry.twist.twist.linear.y = vel.y()/1000;
     odometry.twist.twist.linear.z = vel.z()/1000;
 
-    pub_odometry->publish(odometry);
+    pub_odometry_->publish(odometry);
 
     geometry_msgs::msg::TransformStamped t;
     t.header.stamp = now;
@@ -101,45 +144,7 @@ void VioNode::publish_odometry()
     t.transform.rotation.z = quat.z();
     t.transform.rotation.w = quat.w();
 
-    tf_broadcaster->sendTransform(t);
+    tf_broadcaster_->sendTransform(t);
 }
-void VioNode::img_cb(const sensor_msgs::msg::Image::ConstSharedPtr& cam0_msg, const sensor_msgs::msg::Image::ConstSharedPtr& cam1_msg)
-{       
-        cv_bridge::CvImagePtr cam0_ptr = cv_bridge::toCvCopy(cam0_msg, sensor_msgs::image_encodings::MONO8);
-        cv_bridge::CvImagePtr cam1_ptr = cv_bridge::toCvCopy(cam1_msg, sensor_msgs::image_encodings::MONO8);
-        cv::Mat img0, img1;
-        img0 = cam0_ptr->image;
-        img1 = cam1_ptr->image;
-        time = cam0_msg->header.stamp.sec + cam0_msg->header.stamp.nanosec * (1e-9);
-        cv::Mat visualized_image;
-        stopwatch.start_time();       
-        visualized_image = estimator.inputImage(time, img0, img1);
-        elasped_time = stopwatch.end_time();
-        double processing_time_s = elasped_time / 1000.0;
-        double processing_hz = 1.0 / processing_time_s;
-        RCLCPP_INFO(this->get_logger(), 
-                    "VIO Image Processing Time: %.2f ms (%.2f Hz)", 
-                    elasped_time,
-                    processing_hz);
-        cv_bridge::CvImage out_msg;
-        out_msg.header = cam0_msg->header;
-        out_msg.encoding = sensor_msgs::image_encodings::BGR8; //MONO8
-        out_msg.image = visualized_image;
-        
-        // img_pub->publish(*cam1_msg);
-        img_pub->publish(*out_msg.toImageMsg());
-        // publish for RQT PLOT
-        auto stats = estimator.getLastFeatureStats();
-        auto blue_msg = std_msgs::msg::Int32();
-        auto green_msg = std_msgs::msg::Int32();
-        auto red_msg = std_msgs::msg::Int32();
 
-        blue_msg.data = stats.blue_count;
-        green_msg.data = stats.green_count;
-        red_msg.data = stats.red_count;
-
-        blue_dots_pub->publish(blue_msg);
-        green_dots_pub->publish(green_msg);
-        red_dots_pub->publish(red_msg);
-}
 
